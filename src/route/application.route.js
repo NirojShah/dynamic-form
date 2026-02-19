963c28faf206b9a30de51787bc024e2671633082
Niraj Shah <nirajshah950@gmail.com>
2026-02-19 10:30:00 +0530

    fix: update application route

--- src/route/application.route.js
+++ src/route/application.route.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import formRoute from "../module/form/form.route.js";
 
 const applicationRouter = Router();
+
+applicationRouter.use(formRoute)
 
 
 export default applicationRouter;
