6036b6f7deda5e49932dbf30cb6c780b04fafde4
Niraj Shah <nirajshah950@gmail.com>
2026-03-08 10:30:00 +0530

    feat: update app route

--- src/route/app.js
+++ src/route/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import globalErrorHandler from "../utility/globalErrorHandler.js";
+import applicationRouter from "./application.route.js";
 
 const app = express();
 
+app.use(applicationRouter)
 
 
 app.use(globalErrorHandler);
