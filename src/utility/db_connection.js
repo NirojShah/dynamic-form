a830243ffeaaa99627ad8a5b18d69c10f9576bf0
Niraj Shah <nirajshah950@gmail.com>
2026-03-29 21:48:29 +0530

    Define the mongodb connection function.

--- src/utility/db_connection.js
+++ src/utility/db_connection.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const connectMongo = () => {
-  mongoose.connect();
+  mongoose.connect(process.env.DB).then(()=>{
+    console.log("db connected successfully.")
+  })
+  .catch(err=>{
+    console.log(err.message)
+  })
 };
 
 export default connectMongo;
