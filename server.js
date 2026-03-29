722c44c879dbf08ee9ce92e3b566827cfcfa2231
Niraj Shah <nirajshah950@gmail.com>
2026-03-29 21:50:53 +0530

    call the mongodb)_connection and config_env function for db connection and environment setup.

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import http from "http"
 import app from "./src/route/app.js"
 import connectMongo from "./src/utility/db_connection.js"
+import { config_env } from "./environment_setup.js";
+
+
+config_env();
 
 const PORT = process.env.PORT || 7050;
 const server = http.createServer(app)
 
 connectMongo()
 server.listen(PORT,()=>{
-    console.log("server started")
+    console.log("server started : "+PORT)
 })
 
 
