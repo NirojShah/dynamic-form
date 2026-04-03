ed061904509509e7110f882e51c3ea0e5ed43f5b
Niraj Shah <nirajshah950@gmail.com>
2026-04-03 23:51:49 +0530

    remove the unused comments.

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,21 +2,17 @@ import { Jwt } from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
   try {
-    // 1. Extract token from the cookie
     const token = req.cookies.access_token;
 
     if (!token) {
       return res.status(401).json({ error: "No token, authorization denied" });
     }
-
-    // 2. Verify the token using your secret key
     const decoded = Jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
       return res.status(401).json({ error: "Token is invalid or expired" });
     }
 
-    // 3. Attach user data to the request and proceed
     req.user = decoded;
     next();
   } catch (error) {
