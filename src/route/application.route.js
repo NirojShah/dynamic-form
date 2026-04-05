7872cea403ee13982664490d7b60b7e3cd38a7de
Niraj Shah <nirajshah950@gmail.com>
2026-04-05 21:36:41 +0530

    Add the routes for the user and org.

--- src/route/application.route.js
+++ src/route/application.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import formRoute from "../module/form/form.route.js";
+import userRoute from "../module/user/user.route.js";
+import orgRouter from "../module/organization/organization.route.js";
 
 const applicationRouter = Router();
 
 applicationRouter.use(formRoute)
+applicationRouter.use(userRoute)
+applicationRouter.use(orgRouter)
 
 
 export default applicationRouter;
