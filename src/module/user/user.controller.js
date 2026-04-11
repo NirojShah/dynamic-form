bf7be34780e5077d4f02297cd5e2c255c2c5a279
Niraj Shah <nirajshah950@gmail.com>
2026-04-11 23:01:44 +0530

    Call the process user signup for the controller

--- src/module/user/user.controller.js
+++ src/module/user/user.controller.js
@@ -1,7 +1,26 @@
 import asyncErrorHandler from "../../utility/asyncErrorHandler.js";
+import userService from "./user.service.js";
 
 const createUser = asyncErrorHandler(async (req, res) => {
   const { name, password, email, organizationId } = req.body;
+  const resp = await userService.processSignup({
+    name,
+    email,
+    password,
+    organizationId,
+  });
+
+  if (resp.success) {
+    return res.status(201).json({
+      status: "success",
+      message: "user signedup successfully.",
+    });
+  }
+
+  return res.status(500).json({
+    status: "failed",
+    message: resp.message,
+  });
 });
 
 const login = asyncErrorHandler(async (req, res) => {
