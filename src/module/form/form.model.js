4e2ef5737444dcf182031e69c73657250bd74f0c
Niraj Shah <nirajshah950@gmail.com>
2026-04-13 21:42:58 +0530

    Form Model Update.

--- src/module/form/form.model.js
+++ src/module/form/form.model.js
@@ -1,18 +1,77 @@
 import mongoose from "mongoose";
 
 const FieldSchema = new mongoose.Schema({
-  key: String,              
-  label: String,            
-  type: String,             
-  refSchemaId: mongoose.Schema.Types.ObjectId, 
-  required: Boolean
+  key: {
+    type: String,
+    required: true
+  },
+
+  label: {
+    type: String,
+    required: true
+  },
+
+  type: {
+    type: String,
+    required: true
+    // text, email, number, select, relation etc
+  },
+
+  refSchemaId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schema",
+    default: null
+  },
+
+  required: {
+    type: Boolean,
+    default: false
+  }
+}, { _id: false });
+
+
+const SchemaDefinition = new mongoose.Schema({
+
+  // Form Name
+  name: {
+    type: String,
+    required: true
+  },
+
+  // Belongs to which organization
+  organizationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Organization",
+    required: true
+  },
+
+  // Created by which user
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  // Updated by user
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+
+  // Dynamic fields
+  fields: [FieldSchema],
+
+  // Active or Deleted
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+
+}, {
+  timestamps: true
 });
 
-const SchemaModelDefination = new mongoose.Schema({
-  name: String,
-  fields: [FieldSchema]
-});
 
-const SchemaModel =  mongoose.model("Schema", SchemaModelDefination);
+const SchemaModel = mongoose.model("Schema", SchemaDefinition);
 
 export default SchemaModel;
