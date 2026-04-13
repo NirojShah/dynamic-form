242e60095a0b5748f4234760a4578879a11bb962
Niraj Shah <nirajshah950@gmail.com>
2026-04-13 21:42:29 +0530

    add the route and middleware.

--- src/route/application.route.js
+++ src/route/application.route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import formRoute from "../module/form/form.route.js";
 import userRoute from "../module/user/user.route.js";
 import orgRouter from "../module/organization/organization.route.js";
+import verifyToken from "../middleware/auth.middleware.js";
 
 const applicationRouter = Router();
 
-applicationRouter.use(formRoute)
 applicationRouter.use("/user",userRoute)
+applicationRouter.use(verifyToken)
+applicationRouter.use("/form",formRoute)
 applicationRouter.use("/org",orgRouter)
 
 
