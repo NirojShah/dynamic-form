0cda6ade7d83c264e87c193426ba6c60b58420bc
Niraj Shah <nirajshah950@gmail.com>
2026-04-15 22:21:05 +0530

    Commit.

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,22 +1,22 @@
-import functions from "../funcTesting.js";
+    import functions from "../funcTesting.js";
 
-test('toBe', () => { 
-    expect(functions.add(1,2)).toBe(3);
-});
+    test('toBe', () => { 
+        expect(functions.add(1,2)).toBe(3);
+    });
 
-test("toBeNull", () => {
-    expect(functions.add(1,5)).not.toBe(null);
-});
+    test("toBeNull", () => {
+        expect(functions.add(1,5)).not.toBe(null);
+    });
 
-test("toBeGreaterThan", () => {
-    expect(functions.add(1,5)).toBeGreaterThan(1);
-});
+    test("toBeGreaterThan", () => {
+        expect(functions.add(1,5)).toBeGreaterThan(1);
+    });
 
-test("toBeLessThan", () => {
-    expect(functions.add(1,5)).toBeLessThan(10);
-});
+    test("toBeLessThan", () => {
+        expect(functions.add(1,5)).toBeLessThan(10);
+    });
 
 
-test("toBeCloseTo", () => {
-    expect(functions.add(1,5)).toBeCloseTo(6);
-});
+    test("toBeCloseTo", () => {
+        expect(functions.add(1,5)).toBeCloseTo(6);
+    });
