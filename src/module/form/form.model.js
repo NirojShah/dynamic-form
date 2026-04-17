407f62810037739e485123341668b1d8ee503cf8
Niraj Shah <nirajshah950@gmail.com>
2026-04-17 23:34:57 +0530

    Add the option fields.

--- src/module/form/form.model.js
+++ src/module/form/form.model.js
@@ -1,76 +1,85 @@
 import mongoose from "mongoose";
 
-const FieldSchema = new mongoose.Schema({
-  key: {
-    type: String,
-    required: true
+const FieldSchema = new mongoose.Schema(
+  {
+    key: {
+      type: String,
+      required: true,
+    },
+
+    label: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      required: true,
+      // text, email, number, select, relation etc
+    },
+
+    refSchemaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Schema",
+      default: null,
+    },
+
+    options: [
+      {
+        label: String,
+        value: String,
+      },
+    ],
+
+    required: {
+      type: Boolean,
+      default: false,
+    },
   },
+  { _id: false },
+);
 
-  label: {
-    type: String,
-    required: true
+const SchemaDefinition = new mongoose.Schema(
+  {
+    // Form Name
+    name: {
+      type: String,
+      required: true,
+    },
+
+    // Belongs to which organization
+    organizationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+
+    // Created by which user
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    // Updated by user
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    // Dynamic fields
+    fields: [FieldSchema],
+
+    // Active or Deleted
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
-
-  type: {
-    type: String,
-    required: true
-    // text, email, number, select, relation etc
+  {
+    timestamps: true,
   },
-
-  refSchemaId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Schema",
-    default: null
-  },
-
-  required: {
-    type: Boolean,
-    default: false
-  }
-}, { _id: false });
-
-
-const SchemaDefinition = new mongoose.Schema({
-
-  // Form Name
-  name: {
-    type: String,
-    required: true
-  },
-
-  // Belongs to which organization
-  organizationId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Organization",
-    required: true
-  },
-
-  // Created by which user
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-
-  // Updated by user
-  updatedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  },
-
-  // Dynamic fields
-  fields: [FieldSchema],
-
-  // Active or Deleted
-  isActive: {
-    type: Boolean,
-    default: true
-  }
-
-}, {
-  timestamps: true
-});
-
+);
 
 const SchemaModel = mongoose.model("Schema", SchemaDefinition);
 
