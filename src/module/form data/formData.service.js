cda31e51614e76502763b0118cabccc6c9375d40
Niraj Shah <nirajshah950@gmail.com>
2026-04-20 23:56:48 +0530

    Define the service functions for -
    1. processUploadData
    2. processGetFormData.

---
+++ src/module/form data/formData.service.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import SchemaModel from "../form/form.model.js";
+import UserInput from "./formData.model.js";
+import CustomError from "../../utility/customError.js";
+
+const processUploadData = async ({ formData, formId, organizationId }) => {
+  try {
+    const createdEntry = await UserInput.create({
+      formId,
+      organizationId,
+      data: formData,
+    });
+
+    return {
+      success: true,
+      data: createdEntry,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message,
+    };
+  }
+};
+
+const processGetFormData = async ({ page = 1, limit = 10, formId }) => {
+  if (!formId) {
+    return {
+      success: false,
+      message: "formId is required.",
+    };
+  }
+  const formExists = await SchemaModel.findById(formId);
+
+  if (!formExists) {
+    throw new CustomError(400, "Please send valid formId");
+  }
+
+  const offset = (page - 1) * limit;
+
+  const [data, total] = await Promise.all([
+    UserInput.find({ formId })
+      .skip(offset)
+      .limit(limit)
+      .sort({ createdAt: -1 }),
+
+    UserInput.countDocuments({ formId }),
+  ]);
+
+  return {
+    success: true,
+    data,
+    pagination: {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
+};
+
+const formDataService = {
+  processUploadData,
+  processGetFormData,
+};
+
+export default formDataService;
