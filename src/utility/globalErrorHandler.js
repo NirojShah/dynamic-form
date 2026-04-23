4dc37300d942b5b780c11ddf667fc8c5ee0bbd79
Niraj Shah <nirajshah950@gmail.com>
2026-04-23 23:46:25 +0530

    Eslint error fixxed.

--- src/utility/globalErrorHandler.js
+++ src/utility/globalErrorHandler.js
@@ -1,9 +1,8 @@
-const globalErrorHandler = async(error,req,res,next)=>{
-    console.log(error)
-    return res.status(500).json({
-        success:"failed",
-        message: error.message
-    })
-}
+const globalErrorHandler = async (error, req, res, _next) => {
+  return res.status(500).json({
+    success: "failed",
+    message: error.message,
+  });
+};
 
 export default globalErrorHandler;
