22a197a84b32578606c431b07e13a6b4bc51abeb
Niraj Shah <nirajshah950@gmail.com>
2026-04-23 23:46:53 +0530

    Add semicolun

--- src/utility/asyncErrorHandler.js
+++ src/utility/asyncErrorHandler.js
@@ -1,11 +1,11 @@
 let asyncErrorHandler = (func) => {
     return ((req, res, next) => {
             func(req, res, next).catch((err) => {
-                next(err)
-            })
+                next(err);
+            });
         }
 
-    )
-}
+    );
+};
 
 export default asyncErrorHandler;
