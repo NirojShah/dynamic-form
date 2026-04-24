450c70c423d19d759429d2ab863f698b116f6b53
Niraj Shah <nirajshah950@gmail.com>
2026-04-24 23:25:06 +0530

    Fixed eslint issues.

--- src/route/app.js
+++ src/route/app.js
@@ -1,11 +1,11 @@
-import express from "express"
+import express from "express";
 import globalErrorHandler from "../utility/globalErrorHandler.js";
 import applicationRouter from "./application.route.js";
 
 const app = express();
 
-app.use(express.json())
-app.use("/app/v1",applicationRouter)
+app.use(express.json());
+app.use("/app/v1",applicationRouter);
 
 
 app.use(globalErrorHandler);
