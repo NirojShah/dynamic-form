2ca36f2b3ed3c71e0aa8a916bc3ac797f7895d04
Niraj Shah <nirajshah950@gmail.com>
2026-04-25 23:06:44 +0530

    Implement cookies parser and cors.

--- src/route/app.js
+++ src/route/app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import globalErrorHandler from "../utility/globalErrorHandler.js";
 import applicationRouter from "./application.route.js";
 
 const app = express();
-
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-app.use("/app/v1",applicationRouter);
-
+app.use("/app/v1", applicationRouter);
 
 app.use(globalErrorHandler);
 export default app;
