0a3a6114a907630d8b3ebed52a6489dab1222bd6
Niraj Shah <nirajshah950@gmail.com>
2026-04-27 21:21:50 +0530

    Update the User route and add the route for the /me to check if the user is logged in or not.

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userController from "./user.controller.js";
+import verifyToken from "../../middleware/auth.middleware.js";
 
 const userRoute = Router();
 
-userRoute.post("/admin/signup", userController.createAdminProfile);
-userRoute.post("/signup", userController.createUser);
 userRoute.post("/login", userController.login);
-userRoute.put("/deactivate", userController.deactivateProfile);
-userRoute.put("/update-profile", userController.updateProfile);
+userRoute.get("/me", verifyToken, userController.verifyMe);
+userRoute.post("/admin/signup", userController.createAdminProfile);
+userRoute.post("/signup", verifyToken, userController.createUser);
+userRoute.put("/deactivate", verifyToken, userController.deactivateProfile);
+userRoute.put("/update-profile", verifyToken, userController.updateProfile);
 
 export default userRoute;
