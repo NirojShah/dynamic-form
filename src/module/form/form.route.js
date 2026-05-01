d1b9f803ece08fd10c47447780ab75eac93c648d
Niraj Shah <nirajshah950@gmail.com>
2026-05-01 21:57:50 +0530

    form-detail - fetch the selected form in detail means form with all the fields.

--- src/module/form/form.route.js
+++ src/module/form/form.route.js
@@ -6,5 +6,6 @@ const formRoute = express.Router();
 formRoute.post("/create", formController.createForm);
 formRoute.post("/create-fields", formController.addFileds);
 formRoute.get("/forms", formController.getAllforms);
+formRoute.get("/form-detail/:formId", formController.getformInDetail);
 
 export default formRoute;
