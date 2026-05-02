f35831f1287fdcb5ee59528621f0c3641dfb8a91
Niraj Shah <nirajshah950@gmail.com>
2026-05-02 16:45:12 +0530

    Taking the organizaton id from the req.user.

--- src/module/form/form.controller.js
+++ src/module/form/form.controller.js
@@ -44,6 +44,7 @@ const addFileds = asyncErrorHandler(async (req, res) => {
 });
 
 const getAllforms = asyncErrorHandler(async (req, res) => {
+  const orgainzationId = req.user.organizationId;
   const resp = await formService.processAllForms();
   if (resp.success) {
     return res.status(200).json({
