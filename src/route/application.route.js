56771b2e10454bf3dadb17f0e300f4400b7842a9
Niraj Shah <nirajshah950@gmail.com>
2026-05-06 19:48:00 +0530

    FEAT
    add the public route.

--- src/route/application.route.js
+++ src/route/application.route.js
@@ -3,13 +3,14 @@ import formRoute from "../module/form/form.route.js";
 import userRoute from "../module/user/user.route.js";
 import orgRouter from "../module/organization/organization.route.js";
 import verifyToken from "../middleware/auth.middleware.js";
+import publicFormRoute from "../module/public-form-route/public.form.route.js";
 
 const applicationRouter = Router();
 
-applicationRouter.use("/user",userRoute);
+applicationRouter.use("/user", userRoute);
+applicationRouter.use("/public", publicFormRoute);
 applicationRouter.use(verifyToken);
-applicationRouter.use("/form",formRoute);
-applicationRouter.use("/org",orgRouter);
-
+applicationRouter.use("/form", formRoute);
+applicationRouter.use("/org", orgRouter);
 
 export default applicationRouter;
