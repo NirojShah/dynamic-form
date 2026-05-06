b0d32ccf890d38a536e52bb5eb344db6a1fe560d
Niraj Shah <nirajshah950@gmail.com>
2026-05-06 19:47:12 +0530

    FEAT Implemented the fileUplaod middleare.

--- src/route/app.js
+++ src/route/app.js
@@ -3,14 +3,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import globalErrorHandler from "../utility/globalErrorHandler.js";
 import applicationRouter from "./application.route.js";
+import fileUpload from "express-fileupload";
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.220.1:5173",
+      "http://192.168.11.1:5173/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
+app.use(fileUpload());
+
 app.use("/app/v1", applicationRouter);
 
 app.use(globalErrorHandler);
