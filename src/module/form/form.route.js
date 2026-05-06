ef1e9b634db4eb3c281289cbd1416749a5fe7388
Niraj Shah <nirajshah950@gmail.com>
2026-05-06 19:35:14 +0530

    NEW
    add two new routes
    
    1. get public link of the form.
    
    2. response from the users submitted form with the form key.

--- src/module/form/form.route.js
+++ src/module/form/form.route.js
@@ -8,5 +8,7 @@ formRoute.post("/create-fields", formController.addFileds);
 formRoute.get("/forms", formController.getAllforms);
 formRoute.get("/form-detail/:formId", formController.getformInDetail);
 formRoute.post("/form-fields", formController.createFormWithFields);
+formRoute.post("/get-public-link", formController.generatePublicLink);
+formRoute.get("/response/:key", formController.getResponse);
 
 export default formRoute;
