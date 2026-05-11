99921db7175a7e4eff65d095f11075a8672cc1e7
Niraj Shah <nirajshah950@gmail.com>
2026-05-11 15:19:12 +0530

    Define the service functions for the Dashboard analytics.

--- src/module/dashboard/dashboard.service.js
+++ src/module/dashboard/dashboard.service.js
@@ -3,6 +3,7 @@ import SchemaModel from "../form/form.model";
 
 const processDashboardCards = async ({ orgId, start, end }) => {
   try {
+    console.log(start, end);
     const totalForms = await SchemaModel.countDocuments({
       organizationId: new mongoose.Types.ObjectId(orgId),
     });
@@ -19,8 +20,75 @@ const processDashboardCards = async ({ orgId, start, end }) => {
   }
 };
 
+const processGetResponseAnalytics = async ({ orgId, start, end }) => {
+  try {
+    console.log(orgId, start, end);
+  } catch (error) {
+    return {
+      success: false,
+      messge: error.message,
+    };
+  }
+};
+
+const processGetPerformance = async ({ orgId, start, end }) => {
+  try {
+    console.log(orgId, start, end);
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message,
+    };
+  }
+};
+
+const processGetRecentResponse = async ({ orgId, start, end }) => {
+  try {
+    console.log(orgId, start, end);
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message,
+    };
+  }
+};
+
+const processGetRecentForms = async ({ orgId, limit = 5 }) => {
+  try {
+    const forms = await SchemaModel.aggregate([
+      {
+        $match: {
+          organizationId: orgId,
+        },
+      },
+      {
+        $sort: {
+          createdAt: 1,
+        },
+      },
+      {
+        $limit: limit,
+      },
+    ]);
+
+    return {
+      success: true,
+      data: forms,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message,
+    };
+  }
+};
+
 const dashboardService = {
   processDashboardCards,
+  processGetResponseAnalytics,
+  processGetPerformance,
+  processGetRecentResponse,
+  processGetRecentForms,
 };
 
 export default dashboardService;
