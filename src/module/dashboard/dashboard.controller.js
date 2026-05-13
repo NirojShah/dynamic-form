736552fcdceea77e0f3fde9f6c44c4766690b568
Niraj Shah <nirajshah950@gmail.com>
2026-05-13 22:23:59 +0530

    FEAT - implement the dashboardRecentForm controller.

--- src/module/dashboard/dashboard.controller.js
+++ src/module/dashboard/dashboard.controller.js
@@ -1,4 +1,5 @@
 import asyncErrorHandler from "../../utility/asyncErrorHandler.js";
+import CustomError from "../../utility/customError.js";
 import dashboardService from "./dashboard.service.js";
 
 const dashboardCards = asyncErrorHandler(async (req, res) => {
@@ -20,7 +21,19 @@ const dashboardResponseAnalytics = asyncErrorHandler(async (req, res) => {});
 
 const dashboardPerformance = asyncErrorHandler(async (req, res) => {});
 
-const dashboardRecentforms = asyncErrorHandler(async (req, res) => {});
+const dashboardRecentforms = asyncErrorHandler(async (req, res) => {
+  const orgId = req.user.organizationId;
+  const limit = req.query.limit && 5;
+  const resp = await dashboardService.processGetRecentForms({ orgId, limit });
+
+  if (resp.success) {
+    return res.status(200).json({
+      success: true,
+      data: resp.data,
+    });
+  }
+  throw new CustomError(500, resp.message);
+});
 
 const dashboardRecentResponse = asyncErrorHandler(async (req, res) => {});
 
