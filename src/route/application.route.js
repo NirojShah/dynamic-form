80bee9638c8e839151694922cafbcba3c5910761
Niraj Shah <nirajshah950@gmail.com>
2026-05-16 13:14:36 +0530

    FEATURE -
    add an route for the user-org creation.

--- src/route/application.route.js
+++ src/route/application.route.js
@@ -5,6 +5,7 @@ import orgRouter from "../module/organization/organization.route.js";
 import verifyToken from "../middleware/auth.middleware.js";
 import publicFormRoute from "../module/public-form-route/public.form.route.js";
 import dashboardRoute from "../module/dashboard/dashboard.route.js";
+import userorgRouter from "../module/user-org/user-org.route.js";
 
 const applicationRouter = Router();
 
@@ -14,5 +15,6 @@ applicationRouter.use(verifyToken);
 applicationRouter.use("/form", formRoute);
 applicationRouter.use("/org", orgRouter);
 applicationRouter.use("/analytics", dashboardRoute);
+applicationRouter.use("/user-org", userorgRouter);
 
 export default applicationRouter;
