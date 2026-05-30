691e63902cb8498cee268c4100389331d4c8a441
Niraj Shah <nirajshah950@gmail.com>
2026-05-30 23:05:17 +0530

    UPDATE  - 1. update the logic to get the user response from the db. 2. fetch the data from the new collection based on the template. 2. fetch the data from the formId-organizaitonId collection.

--- src/module/form-data/form.data.service.js
+++ src/module/form-data/form.data.service.js
@@ -2,7 +2,6 @@ import mongoose, { Schema, Types } from "mongoose";
 import fileService from "../file-storage/file.storage.service.js";
 import SchemaModel from "../form/form.model.js";
 import formDataUtility from "./form.data.utility.js";
-import SubmittedResponse from "./form.data.model.js";
 
 const processUploadResponse = async ({ formId, orgId, data }) => {
   try {
@@ -42,51 +41,115 @@ const processUploadResponse = async ({ formId, orgId, data }) => {
   }
 };
 
-
-const processGetUserResponse = async (formId, page = 1, limit = 10) => {
+const processGetUserResponse = async ({
+  formId,
+  organizationId,
+  page = 1,
+  limit = 10,
+}) => {
   try {
-    const skip = (Number(page) - 1) * Number(limit);
+    const collectionName = `${formId}-${organizationId}`;
 
-    const [formResult, responses, total] = await Promise.all([
-      SchemaModel.aggregate([
-        { $match: { _id: new Types.ObjectId(formId) } },
-        {
-          $project: {
-            fields: {
-              $map: {
-                input: "$fields",
-                as: "f",
-                in: { label: "$$f.label", type: "$$f.type" },
+    const collectionExists = await mongoose.connection.db
+      .listCollections({
+        name: collectionName,
+      })
+      .toArray();
+
+    const exists = collectionExists.length > 0;
+
+    const schema = new Schema(
+      {
+        formId: {
+          type: Schema.Types.ObjectId,
+          required: [true, "Form Id is required."],
+        },
+        userResponse: {
+          type: Schema.Types.Mixed,
+          required: [true, "data is required"],
+        },
+      },
+      {
+        timestamps: true,
+      },
+    );
+
+    if (exists) {
+      const respModel =
+        mongoose.models[collectionName] ||
+        mongoose.model(collectionName, schema, collectionName);
+
+      const skip = (Number(page) - 1) * Number(limit);
+
+      const [formResult, responses, total] = await Promise.all([
+        SchemaModel.aggregate([
+          {
+            $match: {
+              _id: new Types.ObjectId(formId),
+            },
+          },
+          {
+            $project: {
+              fields: {
+                $map: {
+                  input: "$fields",
+                  as: "f",
+                  in: {
+                    label: "$$f.label",
+                    type: "$$f.type",
+                  },
+                },
               },
             },
           },
+        ]),
+
+        respModel
+          .find({ formId: new Types.ObjectId(formId) })
+          .skip(skip)
+          .limit(Number(limit))
+          .sort({ createdAt: -1 })
+          .lean(),
+
+        respModel.countDocuments({
+          formId: new Types.ObjectId(formId),
+        }),
+      ]);
+
+      if (!formResult.length) {
+        return {
+          success: false,
+          message: "Form not found.",
+        };
+      }
+
+      return {
+        success: true,
+        data: responses,
+        fields: formResult[0].fields,
+        pagination: {
+          total,
+          page: Number(page),
+          limit: Number(limit),
+          totalPages: Math.ceil(total / Number(limit)),
         },
-      ]),
-      SubmittedResponse.find({ formId })
-        .skip(skip)
-        .limit(Number(limit))
-        .sort({ createdAt: -1 })
-        .lean(),
-      SubmittedResponse.countDocuments({ formId }),
-    ]);
-
-    if (!formResult.length) {
-      return { success: false, message: "Form not found." };
+      };
     }
-
     return {
       success: true,
-      data: responses,
-      fields: formResult[0].fields,
+      data: [],
       pagination: {
-        total,
+        total: 0,
         page: Number(page),
         limit: Number(limit),
-        totalPages: Math.ceil(total / Number(limit)),
+        totalPages: 0,
       },
     };
   } catch (err) {
-    return { success: false, message: err.message };
+    return {
+      success: false,
+      message: err.message,
+    };
   }
 };
 
